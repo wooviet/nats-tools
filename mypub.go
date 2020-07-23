98f53f5c2a774a17ec704ad135dd6301eeb48a0f
huyue <hywooviet@163.com>
2020-07-23 10:23:13 +0800

    使用原子操作更新统计值，可指定起始订阅主题号

--- mypub.go
+++ mypub.go
@@ -7,12 +7,13 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
 func usage() {
-	log.Printf("Usage: mypub [-s server] [-np num_publishers] [-nm num_msgs] [-ms msg_size] <msg>\n")
+	log.Printf("Usage: mypub [-s server] [-np num_publishers] " +
+		"[-nm num_msgs] [-ms msg_size] <msg>\n")
 	flag.PrintDefaults()
 }
 
@@ -22,17 +23,21 @@ func showUsageAndExit(exitcode int) {
 }
 
 const (
-	DefaultNumPubs     = 1
-	DefaultNumSubjects = 10
-	DefaultNumMsgs     = 100000
-	DefaultMessageSize = 128
-	DefaultMsgInterval = 0
+	DefaultNumPubs      = 1
+	DefaultNumSubjects  = 10
+	DefaultStartSubjNum = 0
+	DefaultNumMsgs      = 100000
+	DefaultMessageSize  = 128
+	DefaultMsgInterval  = 0
 )
 
 func main() {
-	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
+	var urls = flag.String("s", nats.DefaultURL,
+		"The nats server URLs (separated by comma)")
 	var numPubs = flag.Int("np", DefaultNumPubs, "Number of publishers")
-	var numSubjects = flag.Int("ns", DefaultNumSubjects, "Number of subject published simultaneously by each publisher")
+	var numSubjects = flag.Int("ns", DefaultNumSubjects,
+		"Number of subject published simultaneously by each publisher")
+	var startSub = flag.Int("ss", DefaultStartSubjNum, "Start subject number")
 	var numMsgs = flag.Int("nm", DefaultNumMsgs, "Number of Messages to Publish")
 	var msgSize = flag.Int("ms", DefaultMessageSize, "Size of the message.")
 	var interval = flag.Int("t", DefaultMsgInterval, "Message interval (ms)")
@@ -62,28 +67,33 @@ func main() {
 	// Connect Options.
 	opts := []nats.Option{nats.Name("NATS Sample Publisher")}
 
-	subj := "subject0"
+	subj := fmt.Sprintf("subject%d", *startSub)
 	totalSubjNum := *numPubs * *numSubjects
 	if totalSubjNum > 1 {
-		subj = fmt.Sprintf("subject0 - subject%d", totalSubjNum-1)
+		subj = fmt.Sprintf("%s - subject%d", subj, totalSubjNum+*startSub-1)
 	}
-	log.Printf("Starting publish [pubs=%d, subjs=%d, msgs=%d, msgsize=%d], subject: %s\n",
+	log.Printf("Starting publish [pubs=%d, subjs=%d, msgs=%d, msgsize=%d], "+
+		"subject: %s (Press enter to end)\n",
 		*numPubs, totalSubjNum, *numMsgs, *msgSize, subj)
 
-	published := Published{}
+	count := int64(0)
+	stat := PublishStat{}
 	totalMsgNum := *numMsgs * totalSubjNum
 	go func() {
-		lastPubNum := int64(0)
+		lastPubNum, lastTimeout := int64(0), int64(0)
 		for range time.Tick(1 * time.Second) {
-			pubNum := published.get()
-			log.Printf("Total: %d, Published: %d, Speed: %.2f MB/s",
-				totalMsgNum, pubNum, float64(pubNum - lastPubNum) * float64(*msgSize) / 1024 / 1024)
+			pubNum := atomic.LoadInt64(&stat.PubNum)
+			timeoutMsgs := atomic.LoadInt64(&stat.TimeoutNum)
+			log.Printf("[%d]Total: %d, Published: %d, Speed: %d Msgs/s %.2f MB/s, "+
+				"Timeout Msgs: Total: %d, %d msgs/s",
+				atomic.LoadInt64(&count), totalMsgNum, pubNum, pubNum-lastPubNum,
+				float64(pubNum-lastPubNum)*float64(*msgSize)/1024/1024,
+				timeoutMsgs, timeoutMsgs-lastTimeout)
 			lastPubNum = pubNum
+			lastTimeout = timeoutMsgs
 		}
 	}()
 
-	var finishwg sync.WaitGroup
-	finishwg.Add(totalSubjNum)
 	start := time.Now()
 	for i := 0; i < *numPubs; i++ {
 		nc, err := nats.Connect(*urls, opts...)
@@ -93,35 +103,25 @@ func main() {
 		defer nc.Close()
 
 		for j := 0; j < *numSubjects; j++ {
-			go runPublisher(nc, &finishwg, *numMsgs, *msgSize,
-				*interval, i * *numSubjects+j, &published)
+			atomic.AddInt64(&count, 1)
+			if atomic.LoadInt64(&count)%10 == 0 {
+				go runPublisher(nc, *numMsgs, *msgSize, *interval,
+					i**numSubjects+j, *startSub, &stat)
+			}
 		}
 	}
-	finishwg.Wait()
+
+	os.Stdin.Read(make([]byte, 1))
 	log.Printf("Total: %d, Published: %d, Used: %v",
-		totalMsgNum, published.get(), time.Since(start))
+		totalMsgNum, atomic.LoadInt64(&stat.PubNum), time.Since(start))
 }
 
-type Published struct {
-	published int64
-	rwMutex sync.RWMutex
+type PublishStat struct {
+	TimeoutNum int64
+	PubNum  int64
 }
 
-func (p *Published) add(n int64) {
-	p.rwMutex.Lock()
-	p.published += n
-	p.rwMutex.Unlock()
-}
-
-func (p *Published) get() int64 {
-	p.rwMutex.RLock()
-	published := p.published
-	p.rwMutex.RUnlock()
-	return published
-}
-
-func runPublisher(nc *nats.Conn, finishwg *sync.WaitGroup,
-	numMsgs, msgSize, interval, num int, published *Published) {
+func runPublisher(nc *nats.Conn, numMsgs, msgSize, interval, num, startSub int, stat *PublishStat) {
 	args := flag.Args()[0]
 	msg := []byte(args)
 	if msgSize > 0 {
@@ -130,23 +130,27 @@ func runPublisher(nc *nats.Conn, finishwg *sync.WaitGroup,
 			msg[i] = args[i]
 		}
 	}
-	subj := "subject" + strconv.Itoa(num)
 	for i := 0; i < numMsgs; i++ {
-		_, err := nc.Request(subj, msg, 2*time.Second)
-		if err != nil {
-			if nc.LastError() != nil {
-				log.Fatalf("%v for request (nc.lastError)", nc.LastError())
+		for j := 0; j < 10; j++ {
+			subj := "subject" + strconv.Itoa(num+j+startSub)
+			_, err := nc.Request(subj, msg, 2*time.Second)
+			if err != nil {
+				if nc.LastError() != nil {
+					// log.Fatalf("%v for request (nc.lastError)", nc.LastError())
+				}
+				// log.Fatalf("%v for request", err)
+				if err == nats.ErrTimeout {
+					atomic.AddInt64(&stat.TimeoutNum, 1)
+				}
 			}
-			log.Fatalf("%v for request", err)
+			/*err := nc.Publish(subj, msg)
+			if err != nil {
+				log.Printf("publish failed, errmsg: %v\n", err)
+				continue
+			}*/
+			atomic.AddInt64(&stat.PubNum, 1)
+			time.Sleep(time.Duration(interval) * time.Millisecond)
 		}
-		/*err := nc.Publish(subj, msg)
-		if err != nil {
-			log.Printf("publish failed, err: %v\n", err)
-			continue
-		}*/
-		published.add(1)
-		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
 	nc.Flush()
-	finishwg.Done()
 }
